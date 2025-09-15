80a17e4922f9d18b2fd28a32ce02d8124e9ea224
V1nnyyy05 <ervinhienz@gmail.com>
2025-09-15 20:51:16 +0800

    SW1

---
+++ SW1/CompositePrimeChecker/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePrimeChecker
+{
+    class Program
+    {
+        static bool Prime(int number)
+        {
+            if (number <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            String inp = Console.ReadLine().Trim();
+            var str = inp.Split();
+
+            List<int> num = new List<int>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                    num.Add(int.Parse(str[i]));
+            }
+
+            Console.WriteLine("");
+            for (int i = 0; i < num.Count; i++)
+            {
+                if (Prime(num[i]))
+                {
+                    Console.WriteLine($"{i + 1}. {num[i]}   Prime");
+                }
+                else
+                {
+                    Console.WriteLine($"{i + 1}. {num[i]}   Composite");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ SW1/dToPesoAndYen/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dToPesoAndYen
+{
+    class Program
+    {
+        static (double peso, double yen) DToPhAndYen(double dollar) {
+
+            double peso = dollar * 57.17;
+            double yen = dollar * 146.67;
+
+            return (peso, yen);
+            
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter currency in ($): ");
+            double dollar = Convert.ToDouble(Console.ReadLine());
+
+            var result = DToPhAndYen(dollar);
+
+            Console.WriteLine("\nPhil Peso (P): " + result.peso);
+            Console.WriteLine("Jpn Yen (Y): " + result.yen);
+
+
+            Console.ReadKey();
+        }
+    }
+}
